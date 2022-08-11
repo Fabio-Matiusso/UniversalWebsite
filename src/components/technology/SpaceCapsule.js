642c6469e4564fb532a4b707a719dec1e4488535
Fabio Matiusso <fabiodclm@gmail.com>
2022-08-11 11:21:11 -0300

    Retaforando erro no component technology

--- src/components/technology/SpaceCapsule.js
+++ src/components/technology/SpaceCapsule.js
@@ -30,9 +30,9 @@ const SpaceCapsule = () => {
         { path: '/spacecapsule', index: 3 },
       ],
       terminology: 'The Terminology',
-      name: 'LAUNCH VEHICLE',
+      name: 'SPACE CAPSULE',
       description:
-        'A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth`s surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it`s quite an awe-inspiring sight on the launch pad!',
+        'A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth`s atmosphere without wings. Our capsule is where you`ll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.',
     },
   ];
 
